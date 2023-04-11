3044c888746888531244684e88499b77ef74901b
John Doe <johndoe@email.com>
2023-04-11 22:01:04 +0700

    1s

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<!DOCTYPE html>
-<html lang="ru_RU">
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Document</title>
-		<link rel="stylesheet" href="styles/master.css">
-	</head>
-	<body>
-      <header class="header">
-				
-			</header>
-	    <script src="scripts/script.css"></script>
-	</body>
-</html>
+
