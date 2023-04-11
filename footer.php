bc776a53b3286dfa09a38a3e0323b8cf7a5b3e66
John Doe <johndoe@email.com>
2023-04-11 23:09:00 +0700

    2slava

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+  </head>
+  <body>
+    <footer id="footer">
+      <div class="container">
+        <div class="inner">
+          <div class="left">
+            
+          </div>
+          <div class="center">
+
+          </div>
+          <div class="right">
+
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
