25fea2995a7fac5ab19541581db2906d40807cbc
John Doe <johndoe@email.com>
2023-04-11 20:37:51 +0700

    startSTART

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru_RU">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+		<link rel="stylesheet" href="styles/master.css">
+	</head>
+	<body>
+		
+	<script src="scripts/script.css"></script>
+	</body>
+</html>
