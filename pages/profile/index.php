e8ba2a73c8bd38016399e87e7c17dcb6a685995e
John Doe <johndoe@email.com>
2023-04-12 16:26:57 +0700

    slavik_007

--- pages/profile/index.php
+++ pages/profile/index.php
@@ -6,6 +6,7 @@
   <title>Profile</title>
   <link rel="stylesheet" href="styles/style.css">
   <link rel="stylesheet" href="../../styles/headerfooter.css">
+  <link rel="stylesheet" href="../../styles/style.css">
 </head>
 <body>
   <?php require_once('../../header.php') ?>
@@ -28,12 +29,42 @@
               <img src="img/map-tag.png" class="icon">
               <div>ававыаыв</div><!--Название города-->
             </a>
+            <div class="tags flex">
+              <p class="tag" style=" color: #FF6060; background: #FFD3BA;">#Дизайнер</p> <!--Теги с БД Цвет выбирай сам) Можно в бд занести-->
+            </div>
             <p class="description">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra arcu ipsum, vitae laoreet odio dignissim sed. Donec ut vestibulum nisi. Nam quis mi tristique, placerat mi eu, efficitur nunc. Morbi tincidunt dui diam, at sollicitudin mi consequat at. Etiam venenatis ac eros maximus varius.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque viverra arcu ipsum, vitae laoreet odio dignissim sed.  Laoreet odio dignissim sed. Donec ut vestibulum nisi. Nam quis mi tristique, placerat mi eu, efficitur nunc. Morbi tincidunt dui diam, at sollicitudin mi consequat at. 
             </p>
             <div class="bottom flex">
               <div class="status">Онлайн<!--Онлайн/Офлайн--></div>
               <a class="go_to" href="#">В профиль заказчика</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section id="module_2">
+      <div class="container">
+        <div class="inner">
+          <h2 class="heading">Портфолио</h2>
+          <div class="wrapper">
+            <div class="toper">
+              <div class="flex">
+                <img src="img/download.png">
+                <a href="#">Добавить</a> <!--Типа добавить элемент портфолио, макет его свёрстан, только файлы добавить)-->
+              </div>
+            </div>
+            <div class="content">
+              <div class="portfolio_item">
+                <img src="#" alt="">
+                <h3>Сайт кондитерской</h3>
+                <p class="description">Нахуй я тут есть АААААА!</p>
+                <div class="tools flex">
+                  <img src="img/settings.png" alt="" class="left">
+                  <img src="img/trash.png" alt="" class="right">
+                </div>
+              </div>
             </div>
           </div>
         </div>
