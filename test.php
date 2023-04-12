a58f1a1b11f9972f1010a5cc2103a59c326921e4
John Doe <johndoe@email.com>
2023-04-12 16:09:35 +0700

    Igor

--- test.php
+++ test.php
@@ -3,6 +3,7 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 	require_once('db_model.php');
+    require_once('functions.php');
 ?>
 <html>
     <head>
@@ -16,7 +17,12 @@
             $db->goResult("SELECT * FROM USERS");
             $arr = $db->goResult("SELECT * FROM USERS");
             var_dump($arr);
-            echo "<br>sus2";
+            echo "<br>sus2<br><br>";
+            $arr = getUserInfo(1);
+            var_dump($arr);
+            echo "<br><br>";
+            $arr = getUserCategories(1);
+            var_dump($arr);
         ?>
 	</body>
 </html>
